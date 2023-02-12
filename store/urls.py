e9c0a5a7052ec36f42b597660b6675b771c64e22
gozygreg <goziennabuife@yahoo.com>
2023-02-12 20:35:00 +0000

    Create view and url for singular product page to get more information about a product

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.store, name='store'),
+    path('product/<slug:slug>/', views.product_info, name='product-info'),
 
 
 ]
--- store/views.py
+++ store/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from . models import Category, Product
+from django.shortcuts import get_object_or_404
 
 
 def store(request):
     all_products = Product.objects.all()
-    context = {'all_products': all_products}
+    context = {
+        'all_products': all_products
+        }
     return render(request, 'store/store.html', context)
 
 
@@ -13,4 +16,10 @@ def categories(request):
     return {'all_categories': all_categories}
 
 
+def product_info(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    context = {
+        'product': product
+    }
+    return render(request, 'store/product-info.html')
 
