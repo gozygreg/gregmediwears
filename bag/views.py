065e219f1b61bc85c9603118523e9726bbddb788
gozygreg <goziennabuife@yahoo.com>
2023-02-14 17:14:12 +0000

    Build shopping bag-summary template

--- bag/views.py
+++ bag/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 
 
 def bag_summary(request):
-    pass
+    return render(request, 'bag/bag-summary.html')
+
 
 def bag_add(request):
     pass
 
+
 def bag_delete(request):
     pass
+
 
 def bag_update(request):
     pass
