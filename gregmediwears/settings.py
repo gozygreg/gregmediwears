8f7fab51d06ba23ba903953dc8336ecb5c684979
gozygreg <goziennabuife@yahoo.com>
2023-02-20 17:24:19 +0000

    delete payment and create account app first

--- gregmediwears/settings.py
+++ gregmediwears/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'store',  # Django app
     'bag',  # Django app
-    'payment',  # Django app
+    'account',  # Django app
     'mathfilters',
 ]
 
