670024e981c769878bf8298bc9cd5c5c0e7f3717
gozygreg <goziennabuife@yahoo.com>
2023-02-26 08:50:36 +0000

    Create checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,61 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
+from store.models import Product
 
-# Create your models here.
+
+class ShippingAddress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    address1 = models.CharField(max_length=80, null=False, blank=False)
+    address2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    country = models.CharField(max_length=40, null=False, blank=False)
+
+    class Meta:
+        verbose_name_plural = 'Shipping Address'
+
+    def __str__(self):
+        return 'Shipping Address - ' + str(self.id)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    shipping_address = models.TextField(max_length=5000)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, null=True, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.IntegerField(null=False, blank=False, default=1)
+
+    def __str__(self):
+        return f'Product id {self.product.id} on order {self.order.order_number}'
