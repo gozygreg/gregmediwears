82b8a653380aca310d86697c668b950fe3cebcae
gozygreg <goziennabuife@yahoo.com>
2023-02-26 09:47:13 +0000

    Added shipping form and customize django admin for checkout app

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import ShippingAddress, Order, OrderItem
 
-# Register your models here.
+
+class OrderItemAdminInline(admin.TabularInline):
+    model = OrderItem
+    readonly_fields = ('price',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date_ordered', 'amount_paid')
+
+    fields = ('user', 'order_number', 'full_name', 'date_ordered', 'email',
+              'shipping_address', 'amount_paid')
+
+    list_display = ('order_number', 'date_ordered', 'full_name', 'amount_paid')
+
+    ordering = ('-date_ordered',)
+
+
+class ShippingAddressAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(ShippingAddress, ShippingAddressAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -51,8 +51,8 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, null=True, blank=False, on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, null=True, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=True, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.IntegerField(null=False, blank=False, default=1)
