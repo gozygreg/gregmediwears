99b1b00f5412e5dbb55145eb2ac2f89b2b109aed
gozygreg <goziennabuife@yahoo.com>
2023-02-28 21:24:59 +0000

    Pre-fill authenticated users with account with their shipping address

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+from .models import ShippingAddress
 
 
 def checkout(request):
+    # users with accounts -- pre-fill the form
+    if request.user.is_authenticated:
+        try:
+            # Authenticated users with shipping information
+            shipping_address = ShippingAddress.objects.get(user=request.user.id)
+            context = {'shipping': shipping_address}
+            return render(request, 'checkout/checkout.html', context=context)
+        except:
+            # Authenticated users without shipping information
+            return render(request, 'checkout/checkout.html')
+    else:
+        # Guest user
+        return render(request, 'checkout/checkout.html')
+
+
+
     return render(request, 'checkout/checkout.html')
 
 
