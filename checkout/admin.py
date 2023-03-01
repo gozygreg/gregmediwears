3774dd579f8f06d413ae697593e31fafbe802a43
gozygreg <goziennabuife@yahoo.com>
2023-03-01 00:19:22 +0000

    Added signal.py file to checkout app

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,44 +1,3 @@
-# from django.contrib import admin
-# from django import forms
-# from .models import ShippingAddress, Order, OrderLineItem
-
-
-# class ShippingForm(forms.ModelForm):
-#     class Meta:
-#         model = ShippingAddress
-#         fields = ['address1', 'address2', 'town_or_city', 'postcode']
-#         exclude = ['user',]
-
-
-# class ShippingAddressAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'address1', 'address2', 'town_or_city', 'postcode',)
-
-
-# class OrderLineItemAdminInline(admin.TabularInline):
-#     form = ShippingForm
-
-
-# class OrderItemAdminInline(admin.TabularInline):
-#     model = OrderLineItem()
-#     readonly_fields = ('lineitem_total',)
-
-
-# class OrderAdmin(admin.ModelAdmin):
-#     inlines = (OrderLineItemAdminInline,)
-
-#     readonly_fields = ('order_number', 'date_ordered', 'order_total', 'grand_total',)
-
-#     fields = ('user', 'order_number', 'full_name', 'date_ordered', 'email',
-#               'shipping_address', 'order_total',)
-
-#     list_display = ('order_number', 'date_ordered', 'full_name', 'order_total', 'grand_total',)
-
-#     ordering = ('-date_ordered',)
-
-
-# admin.site.register(Order, OrderAdmin)
-# admin.site.register(ShippingAddress, ShippingAddressAdmin)
-
 from django.contrib import admin
 from django import forms
 from .models import ShippingAddress, Order, OrderLineItem
@@ -56,8 +15,8 @@ class ShippingAddressAdmin(admin.ModelAdmin):
 
 
 class OrderLineItemAdminInline(admin.TabularInline):
-    form = ShippingForm
     model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
 
 
 class OrderAdmin(admin.ModelAdmin):
@@ -66,7 +25,7 @@ class OrderAdmin(admin.ModelAdmin):
     readonly_fields = ('order_number', 'date_ordered', 'order_total', 'grand_total')
 
     fields = ('user', 'order_number', 'full_name', 'date_ordered', 'email',
-              'shipping_address', 'order_total')
+              'shipping_address', 'order_total',)
 
     list_display = ('order_number', 'date_ordered', 'full_name', 'order_total', 'grand_total')
 
