e5f257b1b114c56bdcbb06fa5d9e6b745b7fc5a9
gozygreg <goziennabuife@yahoo.com>
2023-03-03 11:51:58 +0000

    Fix bug in checkout view. Allow authenticated users with pre-fill shipping form to be able to see this when completing their order

--- checkout/views.py
+++ checkout/views.py
@@ -14,7 +14,49 @@ def checkout(request):
     bag = Bag(request)
     shipping_form = ShippingForm()
 
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    stripe.api_key = stripe_secret_key
+
+    intent = None
+
     if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        address1 = request.POST.get('address1')
+        address2 = request.POST.get('address2')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        postcode = request.POST.get('postcode')
+
+        # All-in-one shipping address
+        shipping_address = (address1 + "\n" + address2 + "\n" + city + "\n" + county + "\n" + country + "\n" +"\n" + postcode)
+
+        # Shipping cart information
+        bag = Bag(request)
+
+        # Get the total price of item
+        total_cost = bag.get_total()
+
+        '''
+        order variations
+        1) Create order - Account users with + without shipping information
+        2) Create order - Guest users without an account
+        '''
+        # 1) Create order - Account users with + without shipping information
+        if request.user.is_authenticated:
+            order = Order.objects.create(full_name=name, email=email, shipping_address=shipping_address, order_total=total_cost, user=request.user)
+            order_id = order.pk
+            for item in bag:
+                OrderLineItem.objects.create(order_id=order_id, product=item['product'], quantity=item['price'], user=request.user)
+
+        # 2) Create order - Guest users  without an account
+        else:
+            order = Order.objects.create(full_name=name, email=email, shipping_address=shipping_address, order_total=total_cost)
+            order_id = order.pk
+            for item in bag:
+                OrderItem.objects.create(order_id=order_id, product=item['product'], quantity=item['qty'], price=item['price'])
+
         # Handle shipping address form submission
         shipping_form = ShippingForm(request.POST)
         if shipping_form.is_valid():
@@ -25,55 +67,50 @@ def checkout(request):
             return redirect(reverse('checkout_success'))
 
         # Handle payment form submission
-        bag = request.session.get('bag', {})
-        form_data = {
-            'full_name': request.POST['full_name'],
-            'email': request.POST['email'],
-            'country': request.POST['country'],
-            'postcode': request.POST['postcode'],
-            'town_or_city': request.POST['town_or_city'],
-            'street_address1': request.POST['street_address1'],
-            'street_address2': request.POST['street_address2'],
-            'county': request.POST['county'],
-        }
-        shipping_form = ShippingForm(form_data)
-        if shipping_form.is_valid():
-            shipping_form.save()
-
-            # Iterate through bag items to create line item
-            bag.__init__(self)
-            return redirect(reverse('checckout_success', args=[order.order_number]))
         else:
-            messages.error(request, 'There was an error with your form.')
+            # Create PaymentIntent
+            order_total = bag.get_total()
+            stripe.api_key = stripe_secret_key
 
-    # If no form submission, check bag contents
-    else:
-        if not bag:
-            messages.error(request, "There's nothing in your bag at the moment")
-            return redirect(reverse('store'))
+            if not stripe_public_key:
+                messages.warning(request, 'Your stripe public key is missing!')
+                return redirect(reverse('store'))
 
-        # Create PaymentIntent
-        order_total = bag.get_total()
-        stripe_public_key = settings.STRIPE_PUBLIC_KEY
-        stripe_secret_key = settings.STRIPE_SECRET_KEY
-        stripe.api_key = settings.STRIPE_SECRET_KEY
-        if not stripe_public_key:
-            messages.warning(request, 'Your stripe public key is missing!')
-            return redirect(reverse('store'))
+            intent = stripe.PaymentIntent.create(
+                amount=int(order_total * 100),
+                currency='usd'
+            )
 
-        intent = stripe.PaymentIntent.create(
-            amount=int(order_total * 100),
-            currency='usd'
-        )
+    # Check bag contents
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('store'))
+
+    try:
+        client_secret = intent.client_secret
+    except AttributeError:
+        client_secret = ''
 
     context = {
         'shipping_form': shipping_form,
         'bag': bag,
         'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
+        'client_secret': client_secret,
     }
 
+    # users with accounts -- pre-fill the form
+    if request.user.is_authenticated:
+        try:
+            # Authenticated users with shipping information
+            shipping_address = ShippingAddress.objects.get(user=request.user.id)
+            context['shipping'] = shipping_address
+        except:
+            # Authenticated users without shipping information
+            pass
+
     return render(request, 'checkout/checkout.html', context=context)
+
+
 
 
 
@@ -93,6 +130,7 @@ def checkout_success(request):
 
 def checkout_failed(request):
     return render(request, 'checkout/checkout-failed.html')
+
 
 
 
