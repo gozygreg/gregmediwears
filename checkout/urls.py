29d801502419c1fd10f228bbe2a673c0fa41edcd
gozygreg <goziennabuife@yahoo.com>
2023-03-10 12:34:46 +0000

    export stripe webhook secret

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,6 +6,7 @@ from .webhooks import webhook
 urlpatterns = [
     path('checkout', views.checkout, name='checkout'),
     path('checkout-success/<order_number>', views.checkout_success, name='checkout-success'),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache-checkout_data'),
     path('checkout-failed', views.checkout_failed, name='checkout-failed'),
     path('wh/', webhook, name='webhook'),
 
--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,12 @@
 from decimal import Decimal
 import stripe
+import json
 
+from django.views.decorators.http import require_POST
 from django.conf import settings
 from django.http import JsonResponse
 from django.contrib import messages
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 
 from bag.bag import Bag
 from checkout.forms import ShippingForm
@@ -14,6 +16,23 @@ from store.models import Product
 stripe_public_key = settings.STRIPE_PUBLIC_KEY
 stripe_secret_key = settings.STRIPE_SECRET_KEY
 stripe.api_key = stripe_secret_key
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
